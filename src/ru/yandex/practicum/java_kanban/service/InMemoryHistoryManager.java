65d0297ff04f7f2ce7177ede28ce3be14aaa8694
Nikolay Druzhinin <northernNikolay@yandex.ru>
2024-12-07 23:57:36 +0300

    [SP_5] fix

--- src/ru/yandex/practicum/java_kanban/service/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/java_kanban/service/InMemoryHistoryManager.java
@@ -14,6 +14,7 @@ public class InMemoryHistoryManager<T extends Task> extends InMemoryTaskManager<
     private final Deque<T> taskHistory;
 
     public InMemoryHistoryManager() {
+        super();
         taskHistory = new ArrayDeque<>();
     }
 
