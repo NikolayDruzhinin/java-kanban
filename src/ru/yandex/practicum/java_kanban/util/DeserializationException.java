2ad36db94eea2820eaacfbdb1bb2441877c77149
Nikolay Druzhinin <northernNikolay@yandex.ru>
2025-01-28 22:40:51 +0300

    sprint_7-solution-in-file-manager/fix_code_style

--- src/ru/yandex/practicum/java_kanban/util/DeserializationException.java
+++ src/ru/yandex/practicum/java_kanban/util/DeserializationException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.java_kanban.util;
 
-public class DeserializationException extends RuntimeException{
+public class DeserializationException extends RuntimeException {
     public DeserializationException(final String msg) {
         super(msg);
     }
